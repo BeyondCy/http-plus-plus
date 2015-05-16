0d18c676cefa09ded4c6eca13541f03f0798b004
zerosum0x0 <zerosum0x0@gmail.com>
2015-05-16 17:13:59 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-/**
- * @file main.cpp
- * @author Sean Dillon
- * @date 05 November 2013
- * @brief Entry point of http server application
- * @param Port number for the server
- */
-
 #include "httpserver.h"
 
 int main(int argc, char* argv[])
